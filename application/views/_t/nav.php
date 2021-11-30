c1096adf2409de9f470f01ecd48b617f71c4399b
Alberto Garay <alberto@loquesea>
2021-11-30 13:13:02 +0100

    Ajustes bugs

--- application/views/_t/nav.php
+++ application/views/_t/nav.php
@@ -1,7 +1,7 @@
 <nav class="container navbar navbar-expand-sm bg-dark navbar-dark rounded">
 
 	<a class="navbar-brand" href="<?=base_url()?>">
-		<img src="<?=base_url()?>assets/img/home.png" alt="INICIO" style="width:40px;">
+		<img src="<?=base_url()?>assets/img/home.jpg" alt="INICIO" style="width:40px;">
 	</a>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
